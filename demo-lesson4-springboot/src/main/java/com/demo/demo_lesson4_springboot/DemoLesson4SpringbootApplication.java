94e2941337ae6d823c13cdb252eb8f9b9050d52d
Hoang Kim Tran <thkim1504@gmail.com>
2026-07-19 21:40:15 +0700

    feat: add demo project for Lesson 4

---
+++ demo-lesson4-springboot/src/main/java/com/demo/demo_lesson4_springboot/DemoLesson4SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.demo.demo_lesson4_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLesson4SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLesson4SpringbootApplication.class, args);
+	}
+
+}
---
+++ demo-lesson4-springboot/src/test/java/com/demo/demo_lesson4_springboot/DemoLesson4SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demo_lesson4_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLesson4SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
