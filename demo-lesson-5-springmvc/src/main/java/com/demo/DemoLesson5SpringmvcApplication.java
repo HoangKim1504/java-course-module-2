38b1c127978e301060e809f71a51a1429dcf3ad4
Hoang Kim Tran <thkim1504@gmail.com>
2026-08-03 12:16:08 +0700

    feat: update config files on demo project for Lesson 5

---
+++ demo-lesson-5-springmvc/src/main/java/com/demo/DemoLesson5SpringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLesson5SpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLesson5SpringmvcApplication.class, args);
+	}
+
+}
---
+++ demo-lesson-5-springmvc/src/test/java/com/demo/demo_lesson_5_springmvc/DemoLesson5SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demo_lesson_5_springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLesson5SpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
